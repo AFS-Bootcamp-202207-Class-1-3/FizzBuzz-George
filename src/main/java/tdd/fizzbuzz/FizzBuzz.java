688227a0541717d0a9358d0bb3717687e5b2a032
CHENGE8 <1911270452@qq.com>
2022-07-21 14:19:34 +0800

    feat:implement countOff and pass should_return_FizzBuzz_when_countOff_given_15

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,11 +5,13 @@ public class FizzBuzz {
     public String countOff(int number) {
         String result = "";
         if (number % 3 == 0) {
-            result =  "Fizz";
-        } else if (number % 5 == 0){
-            result = "Buzz";
-        } else  if (number % 7 == 0) {
-            result = "Whizz";
+            result += "Fizz";
+        }
+        if (number % 5 == 0){
+            result += "Buzz";
+        }
+        if (number % 7 == 0) {
+            result += "Whizz";
         }
         return result.length() == 0 ? String.valueOf(number) : result;
     }
