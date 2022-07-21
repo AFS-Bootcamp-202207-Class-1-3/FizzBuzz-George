7106a09014f9d20927d4beeb2bfea192b0778f9e
CHENGE8 <1911270452@qq.com>
2022-07-21 13:46:48 +0800

    test:add should_return_1_when_countOff_given1 test case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,7 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public String countOff(int number) {
+        return null;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,7 +1,23 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
+    @Test
+    public void should_return_1_when_countOff_given1() {
+
+        //given
+        int number = 1;
+        int expectedResult = 1;
+
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.countOff(number);
+
+        //then
+        assertEquals(expectedResult, result);
+    }
 }
